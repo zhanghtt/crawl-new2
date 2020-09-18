ef61db680743f01e5ab0fd9cb97b49144ca07f1c
dongjicheng <421798321@qq.com>
2020-09-18 18:37:46 +0800

    Merge branch 'master' of https://github.com/dongjicheng/crawl-new2
    # Conflicts:
    #	projects/scrapy_redis_projects/jingdong/jingdong/settings.py

--- projects/scrapy_redis_projects/jingdong/summary.py
+++ projects/scrapy_redis_projects/jingdong/summary.py
@@ -31,7 +31,7 @@ def clean_price(item):
 
 
 with op.DBManger() as m:
-    logger.info("step 1: processing jdprice")
+    print("step 1: processing jdprice")
     pipeline = [
         {
             "$match": {"_status": 0},
@@ -41,7 +41,7 @@ with op.DBManger() as m:
     last_sep = m.get_lasted_collection("jingdong", filter={"name": {"$regex": r"^jdprice20\d\d\d\d\d\d_sep"}})
     for table in m.list_tables(dbname="jingdong",filter={"name": {"$regex": r"^jdprice(20\d\d\d\d\d\d)$"}}):
         if not last_sep or table > last_sep:
-            logger.info("step 1: processing {}".format(table))
+            print("step 1: processing {}".format(table))
             for item in m.read_from(db_collect=("jingdong", table), pipeline=pipeline):
                 if item["id"] in price_dic:
                     price_dic[int(item["id"])]["prices"].append(clean_price(item))
@@ -53,7 +53,7 @@ with op.DBManger() as m:
     #skuids in last result
     last_month_skuids = {}
     last_result = m.get_lasted_collection("jingdong", filter={"name": {"$regex": r"^summary_201905_20\d\d\d\d$"}})
-    logger.info("step 2: processing {}".format(last_result))
+    print("step 2: processing {}".format(last_result))
     last_month = last_result.split("_")[-1]
     for skuid, comments, price,cate_id,brand_id,ziying in m.read_from(db_collect=("jingdong", last_result), out_field=("skuid","comment_{}".format(last_month),"price","cate_id","brand_id","ziying")):
         last_month_skuids[int(skuid)] = {"clean_price":price,"comments":comments, "cate_id":cate_id,"brand_id":brand_id,"ziying":ziying}
@@ -62,7 +62,7 @@ with op.DBManger() as m:
     last_sep = m.get_lasted_collection("jingdong", filter={"name": {"$regex": r"^jdskuid20\d\d\d\d\d\d_sep"}})
     for table in m.list_tables(dbname="jingdong",filter={"name": {"$regex": r"^jdskuid(20\d\d\d\d\d\d)retry\d*$"}}):
         if not last_sep or table > last_sep:
-            logger.info("step 3: processing {}".format(table))
+            print("step 3: processing {}".format(table))
             pipeline = [
                 {
                     "$match": {"_status": 0}
@@ -82,7 +82,7 @@ with op.DBManger() as m:
     last_sep = m.get_lasted_collection("jingdong", filter={"name": {"$regex": r"^jdcomment20\d\d\d\d\d\d_sep"}})
     for table in m.list_tables(dbname="jingdong",filter={"name": {"$regex": r"^jdcomment(20\d\d\d\d\d\d)retry\d*$"}}):
         if not last_sep or table > last_sep:
-            logger.info("step 4: processing {}".format(table))
+            print("step 4: processing {}".format(table))
             pipeline = [
                 {
                     "$match": {
@@ -115,9 +115,10 @@ with op.DBManger() as m:
                         price_item["clean_price"] = 79.90
                         price_item["comments":int(comments)]
                         price_item["type"] = 2
-                    price_item["cate_id"] = skuid_sukid_dict["cate_id"]
-                    price_item["brand_id"] = skuid_sukid_dict["brand_id"]
-                    price_item["ziying"] = skuid_sukid_dict["ziying"]
+                    skuid_sukid_item = skuid_sukid_dict[int(skuid)]
+                    price_item["cate_id"] = skuid_sukid_item["cate_id"]
+                    price_item["brand_id"] = skuid_sukid_item["brand_id"]
+                    price_item["ziying"] = skuid_sukid_item["ziying"]
                 elif int(skuid) in last_month_skuids:
                     if int(skuid) in price_dic:
                         price_item = result_dic[int(skuid)]
@@ -136,9 +137,10 @@ with op.DBManger() as m:
                         price_item["clean_price"] = 79.90
                         price_item["comments":int(comments)]
                         price_item["type"] = 5
-                    price_item["cate_id"] = last_month_skuids["cate_id"]
-                    price_item["brand_id"] = last_month_skuids["brand_id"]
-                    price_item["ziying"] = last_month_skuids["ziying"]
+                    last_month_skuids_item = last_month_skuids[int(skuid)]
+                    price_item["cate_id"] = last_month_skuids_item["cate_id"]
+                    price_item["brand_id"] = last_month_skuids_item["brand_id"]
+                    price_item["ziying"] = last_month_skuids_item["ziying"]
                 else:
                     result_dic[int(skuid)] = {}
                     price_item = result_dic[int(skuid)]
@@ -148,19 +150,19 @@ with op.DBManger() as m:
                     price_item["brand_id"] = "0"
                     price_item["ziying"] = "-1"
                     price_item["type"] = 6
-    logger.info("step 5: processing skuid in last_month_skuids but not in result_dic")
+    print("step 5: processing skuid in last_month_skuids but not in result_dic")
     for skuid in last_month_skuids:
         if skuid not in result_dic:
             result_dic[int(skuid)] = {}
             price_item = result_dic[int(skuid)]
-            price_item["clean_price"] = last_month_price_item["clean_price"]
-            price_item["comments"] = last_month_skuids["comments"]
+            price_item["clean_price"] = last_month_skuids[skuid]["clean_price"]
+            price_item["comments"] = last_month_skuids[skuid]["comments"]
             price_item["cate_id"] = "0,0,0"
             price_item["brand_id"] = "0"
             price_item["ziying"] = "-1"
             price_item["type"] = 7
     out_table = "month" + timeUtil.get_month(deltamonth=1,current_month=last_month)
-    logger.info("step 6: processing writing result to {}".format(out_table))
+    print("step 6: processing writing result to {}".format(out_table))
     buffer = []
     buffer_size = 5000
     for i, k in enumerate(result_dic):
